01e949f63c3e778706b1a63f6d5f1c6c93d7dc60
CarlosValleRamirez <Carlos_Valle_Ramirez@hotmail.com>
2020-11-12 03:51:16 -0600

    Introducción Funciones JS

---
+++ funciones.js
@@ -0,0 +1,11 @@
+function comparar() {
+  const val1 = Number(prompt("Ingresa el primer Valor: "));
+  const val2 = Number(prompt("Ingresa el segundo Valor: "));
+
+  if (val1 > val2) {
+    alert(val1 + " Es Mayor Que " + val2);
+  } else {
+    val1 < val2;
+    alert(val1 + " Es Menor Que " + val2);
+  }
+}
